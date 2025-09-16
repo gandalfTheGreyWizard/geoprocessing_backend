fba01d9d8c80f178965cbae0e2024ebd96587602
Gandlf The Grey <chakraborty.soumik23@gmail.com>
2025-09-16 23:50:13 +0530

    some fixes post the first commit

--- pythonscripts/sql_interfacing_scripts.py
+++ pythonscripts/sql_interfacing_scripts.py
@@ -1,11 +1,19 @@
 import pymysql.cursors
+from dotenv import dotenv_values
+config = dotenv_values('.env')
 
+# making the mysql connection object
 connection = pymysql.connect(
-        host = "localhost",
-        user = "root",
-        password = "example",
+        host = config['MYSQL_HOST'],
+        user = config['MYSQL_USERNAME'],
+        password = config['MYSQL_PASSWORD'],
+        database = config['MYSQL_DATABASE']
         )
-def create_database():
+
+def list_users():
     with connection.cursor() as cursor:
-        sql = "create table users"
-    
+        sql = "select * from Users"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+list_users() 
